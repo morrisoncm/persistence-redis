2e75d7810b70a71280c7ceba161dc718a090d952
Clinton Morrison <morrison.clinton@gmail.com>
2024-07-06 21:49:55 -0400

    Reformat build files and dependencies

--- src/main/java/com/demo/template/domain/User.java
+++ src/main/java/com/demo/template/domain/User.java
@@ -1,7 +1,7 @@
 package com.demo.template.domain;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 public class User {
 
